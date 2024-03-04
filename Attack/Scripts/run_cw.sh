cf8fadb77b84c4f8067526d6e1932e2bbfda6fbd
Rachel <e0235225@u.nus.edu>
2024-03-04 01:25:25 +0000

    ok

--- Attack/Scripts/run_cw.sh
+++ Attack/Scripts/run_cw.sh
@@ -32,3 +32,4 @@ done
 #      port_num=`expr $count + $offset`
 #      CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --master_port=$port_num --use_env main.py --model deit_tiny_patch16_224 --batch-size $batchsize --data-path /mnt/d64c1162-08cc-4571-90a3-04c60b6f6f66/xing/imagenet/imagenet --output_dir /home/xing/robust_transformer/imagenet/files_baseline --use_wandb 1 --project_name 'robust' --job_name cw_baseline_eval --attack 'cw' --eps $i --finetune /home/xing/robust_transformer/imagenet/files_baseline/checkpoint.pth --eval 1
 # done
+# done
